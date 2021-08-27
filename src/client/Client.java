645434065d80272482bd63a7c7bd037f45aacf34
Allan19Prieto <allanbanilla@gmail.com>
2021-08-26 19:50:08 -0600

    "Creando carpetas"

---
+++ src/client/Client.java
@@ -0,0 +1,9 @@
+package client;
+
+import java.io.IOException;
+
+public class Client {
+    public static void main(String[] args) throws IOException{
+        System.out.println("This is the Client");
+    }
+}
